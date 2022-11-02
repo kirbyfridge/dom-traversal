37cd95088a0bcd0e964baae8d3aa07c45ab2b0b3
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-11-01 20:46:35 -0400

    made background white + added 2 Os

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,10 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+$('#root').children('ul').children().css('background-color', 'white');
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+$('#root').children('ul').children().eq(0).append('O');
+
+$('#root').children('ul').children().eq(6).append('O');
+
